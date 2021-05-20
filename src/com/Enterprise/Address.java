b3c56913e81c055fdc8c2cae254c7ecece1a7425
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-05-19 23:44:06 -0400

    added instance variables [street, city, state and zip] and their getters

--- src/com/Enterprise/Address.java
+++ src/com/Enterprise/Address.java
@@ -1,9 +1,34 @@
 package com.Enterprise;
 
 public class Address {
-    private String manufacturerAddress;
-    public Address(String mAddress){
-        manufacturerAddress = mAddress;
+
+    private String street, city, state, zip;
+
+    // parameterized constructor
+    public Address(String street, String city, String state, String zip){
+        this.street=street;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
     }
 
+    // gets the street name
+    public String getStreet(){
+        return street;
+    }
+
+    //gets the city name
+    public String getCity(){
+        return city;
+    }
+
+    //gets the state
+    public String getState(){
+        return state;
+    }
+
+    //gets the zip code
+    public String getZip(){
+        return zip;
+    }
 }

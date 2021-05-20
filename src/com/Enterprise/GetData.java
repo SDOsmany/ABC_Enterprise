2e4fd8dbdfc7f03fa5f19f8f25a806828ddafeab
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-05-19 23:25:52 -0400

    added get double, int, message and string methods

--- src/com/Enterprise/GetData.java
+++ src/com/Enterprise/GetData.java
@@ -1,4 +1,26 @@
 package com.Enterprise;
 
+import javax.swing.JOptionPane;
+
 public class GetData {
+    public static double getDouble(String s)
+    {
+        return Double.parseDouble(getMessage(s));
+    }
+
+    public static int getInt(String s)
+    {
+        return Integer.parseInt(getMessage(s));
+    }
+
+    public static String getMessage(String s)
+    {
+        return JOptionPane.showInputDialog(s);
+    }
+
+    public static String getString(String s)
+    {
+        return JOptionPane.showInputDialog(s);
+    }
+    
 }

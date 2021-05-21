245bae918aeea7820578ea6253ad2bbb72be124f
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-05-21 16:03:29 -0400

    removed the getMessage function

--- src/com/Enterprise/GetData.java
+++ src/com/Enterprise/GetData.java
@@ -5,17 +5,12 @@ import javax.swing.JOptionPane;
 public class GetData {
     public static double getDouble(String s)
     {
-        return Double.parseDouble(getMessage(s));
+        return Double.parseDouble(getString(s));
     }
 
     public static int getInt(String s)
     {
-        return Integer.parseInt(getMessage(s));
-    }
-
-    public static String getMessage(String s)
-    {
-        return JOptionPane.showInputDialog(s);
+        return Integer.parseInt(getString(s));
     }
 
     public static String getString(String s)
